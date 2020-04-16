6155d2a17eedb8b17c49791538470b5f650922e4
BaronLip <baronlip@gmail.com>
2020-04-16 00:28:50 -0500

    Added MathEquation.java file/class. Refactored Main to utilize MathEquation.

--- CalcEngine/src/com/pluralsight/calcengine/Main.java
+++ CalcEngine/src/com/pluralsight/calcengine/Main.java
@@ -4,38 +4,40 @@ public class Main {
 
     public static void main(String[] args) {
 //        // This is the second version using arrays.
-	    double[] leftVals = { 100.0d, 25.0d, 225.0d, 11.0d };
-        double[] rightVals = { 50.0d, 92.0d, 17.0d, 3.0d };
-        char[] opCodes = {'d', 'a', 's', 'm'};
+//	    double[] leftVals = { 100.0d, 25.0d, 225.0d, 11.0d };
+//        double[] rightVals = { 50.0d, 92.0d, 17.0d, 3.0d };
+//        char[] opCodes = {'d', 'a', 's', 'm'};
 //      Creates an array with the same length as opCodes.
-        double[] results = new double[opCodes.length];
+//
+//        double[] results = new double[opCodes.length];
 
-        for (int i = 0; i < opCodes.length; i++) {
-            switch (opCodes[i]) {
-                case 'a':
-                    results[i] = leftVals[i] + rightVals[i];
-                    break;
-                case  's':
-                    results[i] = leftVals[i] - rightVals[i];
-                    break;
-                case 'd':
-                    results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
-                    break;
-                case 'm':
-                    results[i] = leftVals[i] * rightVals[i];
-                    break;
-                default:
-                    System.out.println("Error - invalid opCode");
-                    results[i] = 0.0d;
-                    break;
-            }
-        }
-//    Print out each of the values in the results array.
-        for(double theResult : results) {
+        MathEquation[] equations = new MathEquation[4];
+
+        equations[0] = create(100.0d, 50.0d, 'd');
+        equations[1] = create(25.0d, 92.0d, 'a');
+        equations[2] = create(225.0d, 17.0d, 's');
+        equations[3] = create(11.0d, 3.0, 'm');
+
+
+
+        for(MathEquation equation : equations) {
+            equation.execute();
             System.out.print("results = ");
-            System.out.println(theResult);
+            System.out.println(equation.result);
         }
     }
+
+    public static MathEquation create (double leftVal, double rightVal, char opCode) {
+         MathEquation equation = new MathEquation();
+
+         equation.leftVal = leftVal;
+         equation.rightVal = rightVal;
+         equation.opCode = opCode;
+
+         return equation;
+    }
+
+
 }
 //        // This was the first version of calcengine.
 //        double val1 = 100.0d;
--- CalcEngine/src/com/pluralsight/calcengine/MathEquation.java
+++ CalcEngine/src/com/pluralsight/calcengine/MathEquation.java
@@ -1,4 +1,30 @@
 package com.pluralsight.calcengine;
 
 public class MathEquation {
+    public double leftVal;
+    public double rightVal;
+    public char opCode;
+    public double result;
+
+    public void execute() {
+        switch (opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0.0d ? leftVal / rightVal : 0.0d;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            default:
+                System.out.println("Error - invalid opCode");
+                result = 0.0d;
+                break;
+        }
+    }
+
 }
